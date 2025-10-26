5f5ad1c01a8491e0be3bb9b86329389f257d3cca
asdflkj169 <asdflkj169@gmail.com>
2025-10-26 15:04:58 +0000

    abc409d

--- src/abc/409/d.rs
+++ src/abc/409/d.rs
@@ -1,1 +1,37 @@
-fn main() {}
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        t: usize,
+    }
+    for _ in 0..t {
+        input! {
+            n: usize,
+            s: Chars,
+        }
+        match s.iter().zip(s.iter().skip(1)).position(|(a, b)| a > b) {
+            Some(l) => {
+                let r = s.iter().skip(l + 2).position(|&c| c > s[l]).unwrap_or(n) + l + 2;
+                println!(
+                    "{}{}{}{}",
+                    s[..l].iter().collect::<String>(),
+                    if r < n {
+                        s[l + 1..r].iter().collect::<String>()
+                    } else {
+                        s[l + 1..].iter().collect::<String>()
+                    },
+                    s[l],
+                    if r < n {
+                        s[r..].iter().collect::<String>()
+                    } else {
+                        "".to_string()
+                    }
+                );
+            }
+            None => {
+                println!("{}", s.iter().collect::<String>());
+                continue;
+            }
+        }
+    }
+}
